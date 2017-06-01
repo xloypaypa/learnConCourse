c597beb030325e3484515cebc030bb9194d8b17f
xsu <xsu@thoughtworks.com>
2017-06-01 17:06:11 +0800

    add test.

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+
+    @Test
+    public void test() throws Exception {
+        assertEquals(1, 1);
+    }
+}
